e8cfdd00f0c5fcc1c82b84b0d9b15b12a9591e7c
MonsieurLuge <monsieurluge@gmail.com>
2014-09-01 22:32:38 +0200

    Continue to experiment

--- js/cavewall.js
+++ js/cavewall.js
@@ -17,22 +17,87 @@
  */
 CaveWall.prototype.generate = function() {
     var generatedMap = [];
-    var chanceToStartAlive = 0.25;
+    var chanceToStartAlive = 0.2;
+    var iStart, iEnd, jStart, jEnd;
 
-    // Fill the map with some noise
+    //Fill the map with some noise
     for (var i = 0; i < this.width; i++) {
         generatedMap[i] = [];
 
         for (var j = 0; j < this.height; j++) {
             generatedMap[i][j] = (Math.seededRandom(0, 1) < chanceToStartAlive);
+            // generatedMap[i][j] = false;
         }
     }
 
     // Draw the borders
-    // TODO
+    if (this.idToBinaryString().charAt(1) === '1') {
+        iStart = this.idToBinaryString().charAt(0) === '1' ? 0 : 2;
+        iEnd = this.idToBinaryString().charAt(2) === '1' ? 6 : 8;
+        // Top
+        for (var i = iStart; i < iEnd; i++) {
+            generatedMap[i][0] = true;
+            generatedMap[i][1] = true;
+        }
+    } else {
+        iStart = this.idToBinaryString().charAt(0) === '0' ? 0 : 2;
+        iEnd = this.idToBinaryString().charAt(2) === '0' ? 6 : 8;
+        for (var i = iStart; i < iEnd; i++) {
+            generatedMap[i][0] = false;
+        }
+    }
+
+    if (this.idToBinaryString().charAt(3) === '1') {
+        jStart = this.idToBinaryString().charAt(2) === '1' ? 0 : 2;
+        jEnd = this.idToBinaryString().charAt(4) === '1' ? 6 : 8;
+        // Right
+        for (var j = jStart; j < jEnd; j++) {
+            generatedMap[this.width - 1][j] = true;
+            generatedMap[this.width - 2][j] = true;
+        }
+    } else {
+        jStart = this.idToBinaryString().charAt(2) === '0' ? 0 : 2;
+        jEnd = this.idToBinaryString().charAt(4) === '0' ? 6 : 8;
+        for (var j = jStart; j < jEnd; j++) {
+            generatedMap[this.width - 1][j] = false;
+        }
+    }
+
+    if (this.idToBinaryString().charAt(5) === '1') {
+        iStart = this.idToBinaryString().charAt(6) === '1' ? 0 : 2;
+        iEnd = this.idToBinaryString().charAt(4) === '1' ? 6 : 8;
+        // Bottom
+        for (var i = iStart; i < iEnd; i++) {
+            generatedMap[i][this.height - 1] = true;
+            generatedMap[i][this.height - 2] = true;
+        }
+    } else {
+        iStart = this.idToBinaryString().charAt(6) === '0' ? 0 : 2;
+        iEnd = this.idToBinaryString().charAt(4) === '0' ? 6 : 8;
+        for (var i = iStart; i < iEnd; i++) {
+            generatedMap[i][this.height - 1] = false;
+        }
+    }
+
+    if (this.idToBinaryString().charAt(7) === '1') {
+        jStart = this.idToBinaryString().charAt(0) === '1' ? 0 : 2;
+        jEnd = this.idToBinaryString().charAt(6) === '1' ? 6 : 8;
+        // Left
+        for (var j = jStart; j < jEnd; j++) {
+            generatedMap[0][j] = true;
+            generatedMap[1][j] = true;
+        }
+    } else {
+        jStart = this.idToBinaryString().charAt(0) === '0' ? 0 : 2;
+        jEnd = this.idToBinaryString().charAt(6) === '0' ? 6 : 8;
+        for (var j = jStart; j < jEnd; j++) {
+            generatedMap[0][j] = false;
+        }
+    }
 
     // Run the cellular automaton
     this.map = cellularAutomaton(generatedMap, {generations: 7, cellsToDie: 5, cellsToLive: 4, bordersMatters: true});
+    // this.map = generatedMap.slice(0);
 };
 
 /**
